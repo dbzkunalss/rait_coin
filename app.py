298113a07b15659072a193ac9e03b02f6c810e3a
dbzkunalss <kunalsharma.test@gmail.com>
2020-03-24 04:56:07 +0530

    yes

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ from flask import render_template, request
 from stellar_base.keypair import Keypair
 import json
 
+app = Flask(__name__)
+
 @app.route("/gen_address")
 def gen_address():
     kp = Keypair.random()
     publickey = kp.address().decode()
     seed = kp.seed().decode()
-    return json.dumps({'publickey': publickey, 'seed': seed})
+    return json.dumps({'publickey': publickey, 'seed': seed})    
